515f632cdf266c2684ae10c0564cdc6b35dc5cdb
twenty two <torukas07@gmail.com>
2022-04-26 03:46:28 +0400

    some more refactoring

--- lua/autorun/server/sv_bonus.lua
+++ lua/autorun/server/sv_bonus.lua
@@ -2,21 +2,22 @@ util.AddNetworkString('GiveBonus')
 util.AddNetworkString('OpenMenu')
 
 local function bonusMenu(ply, text)
-    if (string.lower(text) == '!bonus') then
+    if (string.Trim(string.lower(text)) == '!bonus') then
         net.Start('OpenMenu')
         net.Send(ply)
     end
 end
 
 net.Receive('GiveBonus', function(len, ply)
-    if(!file.IsDir('bonus/'..ply:SteamID64(), 'DATA')) then
-        local amount = math.random(bonus.moneyAmount.minAmount, bonus.moneyAmount.maxAmount)
+    local data = net.ReadString()
+    local amount = math.random(bonus.moneyAmount.minAmount, bonus.moneyAmount.maxAmount)
+
+    if (!file.IsDir('bonus/'..ply:SteamID64(), 'DATA')) then
         file.CreateDir('bonus/'..ply:SteamID64())
         ply:addMoney(amount)
-        ply:ChatPrint('[Bonus] Вы получили '..amount..'$')
-        print('[Bonus] '..ply:Nick()..' только что получил '..amount..'$')
+        ply:ChatPrint(bonus.localization.fromServerNotification .. bonus.localization[bonus.language]['messageServer']['youRecieve'] .. amount .. GAMEMODE.Config.currency)
     else
-        ply:ChatPrint('[Bonus] Вы уже получили свой бонус!')
+        ply:ChatPrint(bonus.localization.fromServerNotification .. bonus.localization[bonus.language]['messageServer']['alreadyHaveBonus'])
     end
 end)
 
--- lua/autorun/sh_cfg.lua
+++ lua/autorun/sh_cfg.lua
@@ -1,5 +1,5 @@
 bonus = bonus or {}
-bonus.language = 'ru'
+bonus.language = 'en'
 
 bonus.moneyAmount = {
     minAmount = 5000,
@@ -7,16 +7,27 @@ bonus.moneyAmount = {
 }
 
 bonus.localization = {
+    fromServerNotification = '[Bonus] ',
+
     ['ru'] = {
         menuMainTitle = 'Бонус',
         menuSubTitle = 'Получи свой бонус!',
         menuDescription = 'Рандомное количество денег',
         menuButton = 'Получить бонус',
+        ['messageServer'] = {
+            alreadyHaveBonus = 'Вы уже получили свой бонус!',
+            youRecieve = 'Вы получили '
+        }
     },
+    
     ['en'] = {
         menuMainTitle = 'Bonus',
         menuSubTitle = 'Get your bonus!',
         menuDescription = 'Random amount of money',
         menuButton = 'Get bonus',
+        ['messageServer'] = {
+            alreadyHaveBonus = 'You already have your bonus!',
+            youRecieve = 'You recieve '
+        }
     }
 }
