7c602fac1a594eee9d4df0102eb536d6ec59807c
twenty two <torukas07@gmail.com>
2022-04-26 21:10:12 +0400

    fixes

--- lua/autorun/server/sv_bonus.lua
+++ lua/autorun/server/sv_bonus.lua
@@ -1,6 +1,9 @@
 util.AddNetworkString('GiveBonus')
 util.AddNetworkString('OpenMenu')
 
+local localization = bonus.localization
+local language = bonus.language
+
 local function bonusMenu(ply, text)
     if (string.Trim(string.lower(text)) == '!bonus') then
         net.Start('OpenMenu')
@@ -9,16 +12,16 @@ local function bonusMenu(ply, text)
 end
 
 net.Receive('GiveBonus', function(len, ply)
-    local data = net.ReadString()
+    local player = ply:SteamID64()
     local amount = math.random(bonus.moneyAmount.minAmount, bonus.moneyAmount.maxAmount)
-    local existPlayer = sql.QueryValue('SELECT * FROM bonusPlayers WHERE steamID = "' .. data .. '"')
+    local existPlayer = sql.QueryValue('SELECT * FROM bonusPlayers WHERE steamID = "' .. player .. '"')
 
     if (!existPlayer) then
-        sql.Query('INSERT INTO bonusPlayers VALUES("' .. data .. '", "' .. amount .. '")')
+        sql.Query('INSERT INTO bonusPlayers VALUES("' .. player .. '", "' .. amount .. '")')
         ply:addMoney(amount)
-        ply:ChatPrint(bonus.localization.fromServerNotification .. bonus.localization[bonus.language]['messageServer']['youRecieve'] .. amount .. GAMEMODE.Config.currency)
+        ply:ChatPrint(localization.fromServerNotification .. localization[language]['messageServer']['youRecieve'] .. amount .. GAMEMODE.Config.currency)
     else
-        ply:ChatPrint(bonus.localization.fromServerNotification .. bonus.localization[bonus.language]['messageServer']['alreadyHaveBonus'])
+        ply:ChatPrint(localization.fromServerNotification .. localization[language]['messageServer']['alreadyHaveBonus'])
     end
 end)
 
